80cf2726fdd7fa262b1454c99acd56a77405580f
Canuc80k <Canuc80k@gmail.com>
2022-11-07 10:45:32 +0700

    Clean unused param in FileChooser

--- frontend/src/custom-fields/FileChooser/index.jsx
+++ frontend/src/custom-fields/FileChooser/index.jsx
@@ -4,16 +4,11 @@ import React from 'react'
 import { FormFeedback, FormGroup, Input, Label } from 'reactstrap'
 
 const FileChooser = (props) => {
-  const { field, form, label, type, accept} = props
+  const { field, form, label, type} = props
   const { name, onChange, onBlur } = field
 
   const {errors, touched} = form
-  const showError = errors[name] && touched[name]
-
-  const handleSelectedOptionsChange = (e) => {
-    const files = e.target.files;
-    form.setFieldValue(field.name, files[0]);
-  }
+  const showError = errors[name] && touched[name];
 
   return (
     <FormGroup>
@@ -23,9 +18,8 @@ const FileChooser = (props) => {
         id = {name}
 
         type = {type}
-        onChange = {(e) => handleSelectedOptionsChange(e)}
+        onChange = {(e) => form.setFieldValue(field.name, e.target.files[0])}
 
-        
         accept = "image/*"
         className = {showError ? 'is-invalid' : ''}
       />
@@ -38,12 +32,6 @@ const FileChooser = (props) => {
 FileChooser.propTypes = {
   field: PropTypes.object.isRequired,
   form: PropTypes.object.isRequired,
-
-  label: PropTypes.string,
-}
-
-FileChooser.defaultProps = {
-  label: '',
 }
 
 export default FileChooser
