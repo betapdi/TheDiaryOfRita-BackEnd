d7e5f4b1666170d11af3a00bf05354d8817e3e66
Le Han <lecongquochan@gmail.com>
2022-11-21 09:33:28 +0700

    Redirect to main page when logged in

--- frontend/src/features/Authentication/index.jsx
+++ frontend/src/features/Authentication/index.jsx
@@ -9,12 +9,14 @@ import LoginPage from './pages/Login';
 import RegisterPage from './pages/Register';
 
 const Authentication = (props) => {
+  const authTokens = (localStorage.getItem('authTokens') ? (JSON.parse(localStorage.getItem('authTokens'))): null)
+
   return (
     <div>
       <Routes>
         <Route exact path = '/' element = {<Navigate to = "/auth/login" replace />} />
-        <Route path = "login" element = {<LoginPage />} />
-        <Route path = "register" element = {<RegisterPage />} />
+        <Route path = "login" element = {authTokens != null ? <Navigate to = "/manga" replace /> : <LoginPage />} />
+        <Route path = "register" element = {authTokens != null ? <Navigate to = "/manga" replace /> : <RegisterPage />} />
         <Route path = "*" element = {<p>NONE</p>} />
       </Routes>
     </div>
