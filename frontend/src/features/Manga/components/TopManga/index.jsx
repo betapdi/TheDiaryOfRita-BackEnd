cabc67aebbeabc889b3856c4f2efa2e74914a209
Canuc80k <Canuc80k@gmail.com>
2022-11-26 11:45:09 +0700

    First build TopManga component

--- frontend/src/features/Manga/components/TopManga/index.jsx
+++ frontend/src/features/Manga/components/TopManga/index.jsx
@@ -1,13 +1,69 @@
-import './TopManga.scss';
-import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import { MDBCol, MDBRow } from "mdb-react-ui-kit";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import mangaApi from '../../../../api/mangaApi';
+import { getAllManga } from '../../slices/mangaListSlice';
+import "./TopManga.scss";
 
 const TopManga = () => {
-  return (
-    <div className='topMangaContainer d-flex'>
-      <div className="p-2 flex-shrink-1">Flex item</div>
-      <div className="p-2 w-100">Flex item</div>
-    </div>
-  )
-}
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchMangaList = async () => {
+            try {
+                await mangaApi.getAll();
+                dispatch(getAllManga());
+            } catch (error) {
+                console.log("Failed to fetch manga list: ", error);
+            }
+        };
+
+        fetchMangaList();
+    }, []);
+
+    const mangas = useSelector((state) => state.mangaList);
+    const topManga = mangas.slice(1, 8);
+    console.log(topManga[0]);
+
+    return (
+        <MDBRow className="topMangaContainer">
+            {topManga.length > 0 && (
+                <>
+                    <MDBCol md="5">
+                        <img className="top1Manga" src={process.env.REACT_APP_SERVER_URL + topManga[0].cover}/>
+                    </MDBCol>
+                    <MDBCol md="7" className="otherTopMangaContainer">
+                        <MDBRow className="belowTopMangaContainer">
+                            {topManga.map((manga, index) => {
+                                if (index == 0 || index > 3) return; 
+                                return (
+                                    <MDBCol md="4">
+                                        <img 
+                                          className="otherTopManga" 
+                                          src={process.env.REACT_APP_SERVER_URL + topManga[index].cover}
+                                        />
+                                    </MDBCol>
+                                );
+                            })}
+                        </MDBRow>
+                        <MDBRow className="underTopMangaContainer">
+                            {topManga.map((manga, index) => {
+                                if (index == 0 || index <= 3) return; 
+                                return (
+                                    <MDBCol md="4">
+                                        <img 
+                                          className="otherTopManga" 
+                                          src={process.env.REACT_APP_SERVER_URL + topManga[index].cover}
+                                        />
+                                    </MDBCol>
+                                );
+                            })}
+                        </MDBRow>
+                    </MDBCol>
+                </>
+            )}
+        </MDBRow>
+    );
+};
 
 export default TopManga;
