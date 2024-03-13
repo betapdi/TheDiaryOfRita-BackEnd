d3dc10307547b45b703b6c55ab2f2c7e690c593d
BeTapDi <lecongquochan@gmail.com>
2024-03-14 00:19:09 +0700

    moving FE to new Respitory

--- frontend/src/constances/dev-mode/options/index.js
+++
@@ -1,7 +0,0 @@
-export const TESTING_OPTIONS = [
-    { value: 1, label: 'Romance' },
-    { value: 2, label: 'Comedy' },
-    { value: 3, label: 'Action' },
-    { value: 4, label: 'Horror' },
-    { value: 5, label: 'Isekai' },
-]
