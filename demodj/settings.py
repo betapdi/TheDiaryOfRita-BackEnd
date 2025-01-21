64246d3704de0b6059577e39d3098e44c0c69e11
betapdi <lecongquochan@gmail.com>
2025-01-21 12:26:47 +0700

    integrate postgresql

--- demodj/settings.py
+++ demodj/settings.py
@@ -132,8 +132,12 @@ WSGI_APPLICATION = 'demodj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TheDiaryOfRita',
+        'USER': os.environ.get('POSTGRESQL_USER'),
+        'PASSWORD': os.environ.get('POSTGRESQL_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
